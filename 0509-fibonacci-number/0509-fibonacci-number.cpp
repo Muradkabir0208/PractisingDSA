3b310f954f0370232d8eb0d82c59ef0d5dd74516
GhostZen <110313908+Muradkabir0208@users.noreply.github.com>
2022-12-17 00:14:02 +0530

    Time: 0 ms (100.00%), Space: 6 MB (43.11%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n<2){
+            return n;
+        }
+        int x=0,y=1,z;
+        for(int i=1; i<n; i++){
+            z=x+y;
+            x=y;
+            y=z;
+        }
+        return z;
+    }
+};
